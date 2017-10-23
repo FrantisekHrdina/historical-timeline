54dfdca311e9f7b4a4c7746f1641c32896444f33
Martin Kocisky <mkocisky@gmail.com>
2017-10-23 20:54:22 +0200

    added abstract User and subclasses Teacher and Student, still missing some implementation

---
+++ historical-timeline/src/main/java/cz/muni/fi/pa165/entities/Student.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pa165.entities;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Martin Kocisky
+ */
+
+@Entity
+public class Student extends User {
+    public Student() {
+
+    }
+}
