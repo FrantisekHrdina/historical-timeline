b1fb3b81e761a1c4c7d4bfe3752ad7d1e2489d9d
422167 <422167@mail.muni.cz>
2017-10-25 13:58:14 +0200

    Code style
    Added missing @author annotation
    Formatted code
    Added .gitignore

--- historical-timeline/src/main/java/cz/muni/fi/pa165/entities/Event.java
+++ historical-timeline/src/main/java/cz/muni/fi/pa165/entities/Event.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Objects;
 
+/**
+ * @author Martin Wörgötter
+ */
 public class Event {
 	private Long id;
 	private String name;
@@ -11,7 +14,7 @@ public class Event {
 	private String location;
 	private String description;
 	private byte[] image;
-	
+
 	public Event(Long id, String name, LocalDate date, String location, String description, byte[] image) {
 		this.id = id;
 		this.name = name;
@@ -20,40 +23,51 @@ public class Event {
 		this.description = description;
 		this.image = image;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public LocalDate getDate() {
 		return date;
 	}
+
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public byte[] getImage() {
 		return image;
 	}
+
 	public void setImage(byte[] image) {
 		this.image = image;
 	}
@@ -94,5 +108,4 @@ public class Event {
 			return false;
 		return true;
 	}
-	
 }
