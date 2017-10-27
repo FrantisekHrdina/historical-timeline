d9a395b91e592e9548a6d4ecf1fa061c1d5d82a4
Martin Kocisky <mkocisky@gmail.com>
2017-10-27 19:58:41 +0200

    SeminarGroupDao and EventDaoImpl refactoring to comply with the naming standard.

--- persistence/src/main/java/cz/muni/fi/pa165/dao/EventDaoImpl.java
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/EventDaoImpl.java
@@ -17,51 +17,30 @@ public class EventDaoImpl implements EventDao {
     private EntityManager em;
 
     @Override
-    public void create(Event e) {
+    public void addEvent(Event e) {
         em.persist(e);
     }
 
-    /**
-     * Delete group g from the database.
-     * @param g
-     */
     @Override
-    public void delete(Event e) {
+    public void removeEvent(Event e) {
         em.remove(e);
     }
 
     @Override
-    public void update(Event e) {
+    public void editEvent(Event e) {
         em.merge(e);
     }
 
-    /**
-     * Find group by id and return an object.
-     * @param id of the group
-     * @return
-     */
     @Override
-    public Event findById(Long id) {
+    public Event findEvent(Long id) {
         return em.find(Event.class, id);
     }
 
-    /**
-     * Find group by name and return an object.
-     * @param name of the group
-     * @return
-     */
     @Override
-    public Event findByName(String name) {
-        return em.createQuery("select e FROM Event e where e.name like :name ", Event.class).setParameter("name", name).getFirstResult();
-    }
-
-    /**
-     * Find all groups.
-     * @return
-     */
-    @Override
-    public List<Event> findAll() {
+    public List<Event> findAllEvents() {
         return em.createQuery("select e from Event e", Event.class).getResultList();
     }
+
+
 
 }
--- persistence/src/main/java/cz/muni/fi/pa165/dao/SeminarGroupDao.java
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/SeminarGroupDao.java
@@ -4,48 +4,41 @@ import cz.muni.fi.pa165.entities.Group;
 import java.util.List;
 
 /**
- * This is an interface for the Group Data Objects
+ * This is an interface for the SeminarGroup Data Objects
  * @author Martin Kocisky
  */
 
-public interface GroupDao {
+public interface SeminarGroupDao {
 
     /**
      * Add group g to the databse.
      * @param g
      */
-    public void create(Group g);
+    public void addGroup(Group g);
 
     /**
      * Delete group g from the database.
      * @param g
      */
-    public void delete(Group g);
+    public void removeGroup(Group g);
 
     /**
      * Update group g.
      * @param g
      */
-    public void update(Group g);
+    public void editGroup(Group g);
 
     /**
      * Find group by id and return an object.
      * @param id of the group
      * @return
      */
-    public Group findById(Long id);
+    public Group findGroup(Long id);
 
     /**
-     * Find group by name and return an object.
-     * @param name of the group
+     * Find all groups and return them.
      * @return
      */
-    public Group findByName(String name);
-
-    /**
-     * Find all groups.
-     * @return
-     */
-    public List<Group> findAll();
+    public List<Group> findAllGroups();
 
 }
