2dc4e1d5bba4b15ab3bebc52c14d50445520b2a6
František Hrdina <franta.hrdina@mail.muni.cz>
2017-10-29 18:31:49 +0100

    Fix some methods in SeminarGroupDaoImpl

--- persistence/src/main/java/cz/muni/fi/pa165/dao/SeminarGroupDaoImpl.java
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/SeminarGroupDaoImpl.java
@@ -54,6 +54,7 @@ public class SeminarGroupDaoImpl implements SeminarGroupDao {
 		if (g.getName() == null) {
 			throw new IllegalArgumentException("Given group has null name");
 		}
+
 		em.remove(g);
 	}
 
@@ -73,6 +74,10 @@ public class SeminarGroupDaoImpl implements SeminarGroupDao {
 		if (g.getName() == null) {
 			throw new IllegalArgumentException("Given group has null name");
 		}
+
+		if (!em.contains(g)){
+            throw new IllegalArgumentException("Given group is not in db");
+        }
 		em.merge(g);
 	}
 
