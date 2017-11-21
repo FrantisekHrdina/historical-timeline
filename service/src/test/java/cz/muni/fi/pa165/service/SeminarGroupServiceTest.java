1150086a93dbf7dab144b767c92551bf2b39c0a6
Martin Wörgötter <martin.worgotter@gmail.com>
2017-11-21 21:33:17 +0100

    SeminarGroup service tests
    Implements a single test for saving groups.

--- service/src/test/java/cz/muni/fi/pa165/service/SeminarGroupServiceTest.java
+++ service/src/test/java/cz/muni/fi/pa165/service/SeminarGroupServiceTest.java
@@ -1,10 +1,24 @@
 package cz.muni.fi.pa165.service;
 
+import org.hamcrest.core.IsEqual;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import cz.muni.fi.pa165.dao.SeminarGroupDao;
+import cz.muni.fi.pa165.entities.SeminarGroup;
+
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class SeminarGroupServiceTest {
 
@@ -14,5 +28,19 @@ public class SeminarGroupServiceTest {
 	@Autowired
     @InjectMocks
     SeminarGroupService seminarGroupService;
+
+	private SeminarGroup basicGroup;
 	
+	@BeforeMethod
+	public void createSeminarGroups() {
+		SeminarGroup basicGroup = new SeminarGroup();
+		basicGroup.setName("Basic");
+	}
+	
+	@Test
+	public void saveGroup() {
+		when(seminarGroupDao.findGroup(any())).thenReturn(basicGroup);
+		seminarGroupService.saveGroup(basicGroup);
+		assertThat(seminarGroupDao.findGroup(1l)).isEqualTo(basicGroup);
+	}
 }
