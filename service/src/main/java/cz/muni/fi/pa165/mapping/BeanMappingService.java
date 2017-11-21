926a242810e38b49a270cad1d9971c43fbcdfe56
Martin Kocisky <mkocisky@gmail.com>
2017-11-21 15:22:50 +0100

    bean mapping and dozer

--- service/src/main/java/cz/muni/fi/pa165/mapping/BeanMappingService.java
+++ service/src/main/java/cz/muni/fi/pa165/mapping/BeanMappingService.java
@@ -1,7 +1,18 @@
 package cz.muni.fi.pa165.mapping;
 
+import org.dozer.Mapper;
+
+import java.util.Collection;
+import java.util.List;
+
 /**
  * @author František Hrdina
  */
-public class BeanMappingService {
+public interface BeanMappingService {
+
+    public  <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
+
+    public  <T> T mapTo(Object u, Class<T> mapToClass);
+
+    public Mapper getMapper();
 }
