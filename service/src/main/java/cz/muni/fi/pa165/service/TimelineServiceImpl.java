2f89576fcacd9bba6f91fd3121c4efd14f01be0b
Martin Kocisky <mkocisky@gmail.com>
2017-11-21 15:30:33 +0100

    TODO: implement service

--- service/src/main/java/cz/muni/fi/pa165/service/TimelineServiceImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/TimelineServiceImpl.java
@@ -1,8 +1,56 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.entities.Timeline;
+
+import java.util.List;
+
 /**
  *
  * @author Martin Kocisky, 421131
  */
-public class TimelineServiceImpl {
+public class TimelineServiceImpl implements TimelineService {
+    @Override
+    public Long createTimeline(Timeline t) {
+        return null;
+    }
+
+    @Override
+    public void addComment(Long timelineId, String comment) {
+
+    }
+
+    @Override
+    public void setSeminarGroup(Long timelineId, Long seminarGroupId) {
+
+    }
+
+    @Override
+    public void removeSeminarGroup(Long timelineId) {
+
+    }
+
+    @Override
+    public void addEvent(Long timelineId, Long eventId) {
+
+    }
+
+    @Override
+    public void removeEvent(Long timelineId, Long eventId) {
+
+    }
+
+    @Override
+    public Timeline getTimelineById(Long timelineId) {
+        return null;
+    }
+
+    @Override
+    public List<Timeline> getAllTimelines() {
+        return null;
+    }
+
+    @Override
+    public void deleteTimeline(Long timelineId) {
+
+    }
 }
