11c672c2d095e97573de240e1cd6e034ac437590
xbujdos <xbujdos@fi.mini.cz>
2017-11-27 10:55:38 +0100

    User service exception handling adde User service tests implemented

--- service/src/main/java/cz/muni/fi/pa165/service/UserServiceImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/UserServiceImpl.java
@@ -2,13 +2,16 @@ package cz.muni.fi.pa165.service;
 
 import cz.muni.fi.pa165.dao.UserDao;
 import cz.muni.fi.pa165.entities.User;
+import cz.muni.fi.pa165.exception.DAOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Tibor Bujdoš
  */
+@Service
 public class UserServiceImpl implements UserService {
     
     @Autowired
@@ -16,32 +19,65 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void addUser(User user) {
-        userDao.addUser(user);
+        if (user == null){
+            throw new DAOException("User must not be null");
+        }
+        try{
+            userDao.addUser(user);
+        } catch (Exception e) {
+            throw new DAOException("Persistance error occured", e);
+        }
     }
 
     @Override
     public void removeUser(User user) {
-        userDao.removeUser(user);
+        if (user == null){
+            throw new DAOException("User must not be null");
+        }
+        try{
+            userDao.removeUser(user);
+        } catch (Exception e) {
+            throw new DAOException("Persistance error occured", e);
+        }
     }
 
     @Override
     public void editUser(User user) {
-        userDao.editUser(user);
+        if (user == null){
+            throw new DAOException("User must not be null");
+        }
+        try{
+            userDao.editUser(user);
+        } catch (Exception e) {
+            throw new DAOException("Persistance error occured", e);
+        }
     }
 
     @Override
     public User findUser(Long id) {
-        return userDao.findUser(id);
+        try {
+            return userDao.findUser(id);
+        } catch (Exception e) {
+            throw new DAOException("Persistance error occured", e);
+        }
     }
 
     @Override
     public List<User> findAllStudents() {
-        return userDao.findAllStudents();
+        try {
+            return userDao.findAllStudents();
+        } catch (Exception e) {
+            throw new DAOException("Persistance error occured", e);
+        }
     }
 
     @Override
     public List<User> findAllTeachers() {
-        return userDao.findAllTeachers();
+        try {
+            return userDao.findAllTeachers();
+        } catch (Exception e) {
+            throw new DAOException("Persistance error occured", e);
+        }
     }
     
 }
---
+++ service/src/test/java/cz/muni/fi/pa165/service/UserServiceTest.java
@@ -0,0 +1,149 @@
+package cz.muni.fi.pa165.service;
+
+import cz.muni.fi.pa165.configuration.ServiceConfiguration;
+import cz.muni.fi.pa165.dao.UserDao;
+import cz.muni.fi.pa165.entities.User;
+import cz.muni.fi.pa165.exception.DAOException;
+import cz.muni.fi.pa165.service.UserServiceImpl;
+import javax.validation.ConstraintViolationException;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Tibor Bujdoš
+ */
+//@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class UserServiceTest extends AbstractTransactionalTestNGSpringContextTests{
+    
+    @Mock
+    private UserDao userDao;
+    
+    @Autowired
+    private UserServiceImpl userService;
+    
+    private User user1;
+    private User user2;
+    private User user3;
+    
+    @BeforeClass
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);	
+    }
+    
+    @BeforeMethod
+    public void setUp() {
+        user1 = new User("John", 
+                "Snow", 
+                "JohnSnow@winterfell.com", 
+                "youknownothingjohnsnow", 
+                false);
+        
+        user2 = new User("khaleesi",
+                "Dragonborn",
+                "Khaleesi@westeros.com",
+                "dracarys",
+                false);
+        
+        user3 = new User("Cersei",
+                "Lannister",
+                "CerseiLannister@kingslanding.com",
+                "shameshameshame",
+                true);
+                
+        userService.addUser(user2);
+        userService.addUser(user3);
+    }
+    
+    @Test(expectedExceptions = DAOException.class)
+    public void addNullUser() {
+        userService.addUser(null);
+    }
+    
+    @Test(expectedExceptions = DAOException.class)
+    public void editNullUser() {
+        userService.editUser(null);
+    }
+    
+    @Test(expectedExceptions = DAOException.class)
+    public void removeNullUser() {
+        userService.removeUser(null);
+    }
+    
+    @Test
+    public void addUser() {
+        assertThat(user1.getId()).isNull();
+        userService.addUser(user1);
+        assertThat(user1.getId()).isNotNull();
+        assertThat(userService.findUser(user1.getId())).hasFieldOrPropertyWithValue("surname", "Snow");
+    }
+    
+    @Test
+    public void addSameUserTwice() {
+        userService.addUser(user1);
+        userService.addUser(user1);
+        assertThat(userService.findAllStudents()).usingFieldByFieldElementComparator().contains(user1);
+        //user2 is already stored and is student that is why 2
+        assertThat(userService.findAllStudents().size()).isEqualTo(2);
+    }
+    
+    @Test(expectedExceptions = DAOException.class)
+    public void addUserWithNullForename() {
+        user1.setForename(null);
+        userService.addUser(user1);
+    }
+    
+    @Test(expectedExceptions = DAOException.class)
+    public void addUserWithNullSurname() {
+        user1.setSurname(null);
+        userService.addUser(user1);
+    }
+    
+    @Test(expectedExceptions = DAOException.class)
+    public void addUserWithNullEmail() {
+        user1.setEmail(null);
+        userService.addUser(user1);
+    }
+    
+    @Test
+    public void removeUser() {
+        userService.removeUser(user2);
+        assertThat(userService.findAllStudents()).isEmpty();
+        assertThat(userService.findAllTeachers()).usingFieldByFieldElementComparator().containsOnly(user3);
+    }
+    
+    @Test
+    public void editUser() {
+        userService.addUser(user1);
+        user1.setSurname("Targaryen");
+        userService.editUser(user1);
+        assertThat(userService.findUser(user1.getId())).hasFieldOrPropertyWithValue("surname", "Targaryen");
+    }
+    
+    @Test
+    public void findAllStudents() {
+        assertThat(userService.findAllStudents())
+                .usingFieldByFieldElementComparator()
+                .containsOnly(user2);
+    }
+    @Test
+    public void findAllTeachers() {
+        assertThat(userService.findAllTeachers())
+                .usingFieldByFieldElementComparator()
+                .containsOnly(user3);
+    }
+}
