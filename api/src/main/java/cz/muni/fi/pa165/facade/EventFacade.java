aa231480ae6e1ccc330dbe2967decce4d161187f
František Hrdina <franta.hrdina@mail.muni.cz>
2017-11-27 15:28:01 +0100

    Started docs

--- api/src/main/java/cz/muni/fi/pa165/facade/EventFacade.java
+++ api/src/main/java/cz/muni/fi/pa165/facade/EventFacade.java
@@ -9,11 +9,52 @@ import java.util.List;
  * @author František Hrdina
  */
 public interface EventFacade {
+
+    /**
+     * Add event
+     *
+     * @param event Event to be added
+     * @return
+     */
     Long addEvent(EventDTO event);
+
+    /**
+     *
+     * @param id
+     */
     void removeEvent(Long id);
+
+    /**
+     *
+     * @param event
+     */
     void editEvent(EventDTO event);
+
+    /**
+     *
+     * @param id
+     * @return
+     */
     EventDTO findEvent (Long id);
+
+    /**
+     *
+     * @return
+     */
     List<EventDTO> findAllEvents();
+
+    /**
+     *
+     * @param from
+     * @param to
+     * @return
+     */
     List<EventDTO> findEventsInRange(LocalDate from, LocalDate to);
+
+    /**
+     *
+     * @param location
+     * @return
+     */
     List<EventDTO> findEventsByLocation(String location);
 }
