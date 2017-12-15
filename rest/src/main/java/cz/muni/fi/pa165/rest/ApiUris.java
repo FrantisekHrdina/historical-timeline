703c2c84cedef81bb823b2f4243d2579d5c5bf27
Martin Kocisky <mkocisky@gmail.com>
2017-12-15 20:17:24 +0100

    added Timelines Controller

--- rest/src/main/java/cz/muni/fi/pa165/rest/ApiUris.java
+++ rest/src/main/java/cz/muni/fi/pa165/rest/ApiUris.java
@@ -7,4 +7,5 @@ package cz.muni.fi.pa165.rest;
  */
 public abstract class ApiUris {
     public static final String ROOT_URI_EVENTS   = "/events";
+    public static final String ROOT_URI_TIMELINES = "/timelines";
 }
