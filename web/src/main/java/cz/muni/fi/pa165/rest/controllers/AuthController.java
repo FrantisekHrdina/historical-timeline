c6edad884dcfd2c6859be075e1b4793316279df8
martin-wo <martin.worgotter@gmail.com>
2018-01-07 23:01:14 +0100

    Authorization to timelines (#158)
    Student has only access to authorized timelines. Teacher has full
    access.
    Implemented simple logout with redirection to login.

--- web/src/main/java/cz/muni/fi/pa165/rest/controllers/AuthController.java
+++ web/src/main/java/cz/muni/fi/pa165/rest/controllers/AuthController.java
@@ -2,6 +2,7 @@ package cz.muni.fi.pa165.rest.controllers;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import cz.muni.fi.pa165.dto.CredentialsDTO;
+import cz.muni.fi.pa165.dto.TimelineDTO;
 import cz.muni.fi.pa165.dto.UserDTO;
+import cz.muni.fi.pa165.facade.TimelineFacade;
 import cz.muni.fi.pa165.facade.UserFacade;
 import cz.muni.fi.pa165.rest.ApiUris;
 import cz.muni.fi.pa165.rest.exceptions.ResourceNotFoundException;
@@ -27,67 +30,86 @@ import cz.muni.fi.pa165.rest.exceptions.ResourceNotFoundException;
 @RestController
 @RequestMapping(ApiUris.ROOT_URI_AUTH)
 public class AuthController {
-	
+
+	public class AuthContainer {
+		private String role;
+		private List<TimelineDTO> timelines;
+		private String username;
+
+		public AuthContainer(String role, List<TimelineDTO> timelines,
+				String username) {
+			this.role = role;
+			this.timelines = timelines;
+			this.username = username;
+		}
+
+		public List<TimelineDTO> getTimelines() {
+			return timelines;
+		}
+
+		public void setTimelines(List<TimelineDTO> timelines) {
+			this.timelines = timelines;
+		}
+
+		public String getRole() {
+			return role;
+		}
+
+		public void setRole(String role) {
+			this.role = role;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+	}
+
 	@Inject
 	UserFacade userFacade;
-	
+
+	@Inject
+	TimelineFacade timelineFacade;
+
 	final static Logger logger = LoggerFactory.getLogger(AuthController.class);
-	
-	@RequestMapping(method = RequestMethod.POST,
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-	public final String authenticate(@RequestBody CredentialsDTO credentials) {
-		
+
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public final AuthContainer authenticate(
+			@RequestBody CredentialsDTO credentials) {
+
 		UserDTO user = userFacade.findUserByEmail(credentials.getLogin());
-		
-        MessageDigest messageDigest;
-        
-        try {
+
+		MessageDigest messageDigest;
+
+		try {
 			messageDigest = MessageDigest.getInstance("SHA-256");
 			messageDigest.update(credentials.getPassword().getBytes());
-	        String hash = new String(messageDigest.digest());
-	        if (user != null) {
+			String hash = new String(messageDigest.digest());
+			if (user != null) {
 				if (hash.equals(user.getPasswordHash())) {
-					return user.isIsTeacher() ? "teacher" : "student"; 
+					List<TimelineDTO> timelines = user.isIsTeacher()
+							? timelineFacade.getAllTimelines()
+							: timelineFacade
+									.findTimelinesByUserEmail(user.getEmail());
+					String username = user.getForename() + ' '
+							+ user.getSurname();
+					return new AuthContainer(
+							user.isIsTeacher() ? "teacher" : "student",
+							timelines, username);
 				} else {
 					throw new ResourceNotFoundException("Bad password.");
 				}
-	        } else {
-	        	throw new ResourceNotFoundException("Bad login.");
-	        }
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return null;
-	}
-}
-/*
-		try {
-			messageDigest = MessageDigest.getInstance("SHA-256");
-			messageDigest.update("1234".getBytes());
-	        String studentPwd = new String(messageDigest.digest());
-	        messageDigest.update("4321".getBytes());
-	        String teacherPwd = new String(messageDigest.digest());
-
-			messageDigest.update(credentials.getPassword().getBytes());
-	        String hash = new String(messageDigest.digest());
-	        if (credentials.getLogin().equals("student")) {
-				if (hash.equals(studentPwd)) {
-					return "student"; 
-				}
-	        } else if (hash.equals(teacherPwd)) {
-				return "teacher";
 			} else {
-				throw new ResourceNotFoundException("Bad credentials.");
+				throw new ResourceNotFoundException("Bad login.");
 			}
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
-	
 	}
-	*/
+}
--- web/src/main/webapp/auth_controller.js
+++ web/src/main/webapp/auth_controller.js
@@ -13,9 +13,13 @@ auth.controller('LoginController', function ($scope, $rootScope, $http, $locatio
           data: credentials
       }).then(function success(response) {
           console.log('signed in user using ' + credentials.login);
-          $rootScope.userRole = response.data;
-          $cookieStore.put('userRole', response.data);
-          $location.path('timelines');
+          $rootScope.userRole = response.data.role;
+          $rootScope.authorizedTimelines = response.data.timelines;
+          $rootScope.username = response.data.username;
+          $cookieStore.put('userRole', response.data.role);
+          $cookieStore.put('authorizedTimelines', response.data.timelines);
+          $cookieStore.put('username', response.data.username);
+          $location.path('dashboard');
       }, function error(response) {
           console.log('error signing in user ' + credentials.login);
           $rootScope.errorAlert = 'Could not sign in. Please check username and password.';
